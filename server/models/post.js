9f2b8a5acb68b2e68a6056679a5ddfb259c6b1f6
MohamadTarhini84 <moudi55.mt@gmail.com>
2023-01-25 13:34:54 +0200

    posts route configured successfuly

--- server/models/post.js
+++ server/models/post.js
@@ -1,21 +1,29 @@
 const mongoose=require('mongoose')
 
 const postSchema=new mongoose.Schema({
-    username: {
-        type: String,
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
         required: true
     },
     content: {
         type: String,
-        required: true
+        required: true,
+        enum:['text', 'image', 'video', 'voice']
+    },
+    username: {
+        type: String,
+    },
+    caption:{
+        type:String,
     },
     usersWhoLiked: [{
+        _id: false,
         username: String,
-        id: mongoose.Schema.Types.ObjectId
+        id: mongoose.Schema.Types.ObjectId,
     }],
     media: [{
         id: String
-    }]
-})
+    }],
+},{timestamps:true})
 
 module.exports = mongoose.model('post', postSchema)
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -9,77 +9,78 @@ function handleErrors(error){
     Object.values(error.errors).forEach(({properties})=>{
         err[properties.path]=properties.message
     })
-
     return err
 }
 
 router.get('/:userId', (req,res)=>{
-    try{
-        // const posts=getPosts(req.params.userId) //get posts of users connected to user with id of ":userId"
-        res.status(200).json(posts)
-    } catch{
-        res.status(400)
-    }
+    getPosts(req.params.userId, res)
 })
 
-router.post('/new/:userId', upload.single('image'), (req,res)=>{
-    try{
-        createPost(req.body, req.params.userId)
-        res.status(201)
-    } catch(error){
-        res.status(401).send(error)
-    }
+router.post('/new/:userId',  (req,res)=>{
+    createPost(req.body, req.params.userId, res)
 })
 
 router.delete('/delete/:postId',(req,res)=>{
-    try{
-        deletePost(req.params.postId)
-        res.status(202)
-    } catch{
-        res.status(402)
-    }
+    deletePost(req.params.postId, res)
+})
+
+router.patch('/edit/:postId', (req, res)=>{
+    editPost(req.body, req.params.postId, res)
 })
 
 router.patch('/addLike/:postId', (req, res)=>{
-    try{
-        editPost(req.body,req.params.postId)
-        res.status(203)
-    } catch{
-        res.status(403)
-    }
+    addLike(req.body,req.params.postId, res)
 })
 
-// async function getPosts(){
+router.patch('/removeLike/:postId', (req, res)=>{
+    removeLike(req.body,req.params.postId, res)
+})
 
-// }
-
-async function createPost(userObj, res){ 
+async function getPosts(userId, res){
     try{
-        const user = await Post.create(userObj)
+        const posts = await Post.find({userId: mongoose.Types.ObjectId(userId)})
+        res.status(201).json(posts)
+    } catch (error){
+        const errors= handleErrors(error)
+        res.status(401).json({errors})
+        res.status(401)
+    }
+}
+
+async function createPost(userObj, userId, res){ 
+    try{
+        const newPost= new Post({
+            userId: mongoose.Types.ObjectId(userId.trim()),
+            content: userObj.content,
+            username:userObj.username,
+            caption:userObj.caption
+        })
+        const user = await Post.create(newPost)
         res.status(201).json(user)
     } catch (error){
         const errors= handleErrors(error)
         res.status(401).json({errors})
+        res.status(401)
     }
 }
 
-async function deletePost(userId){
+async function deletePost(userId, res){
     try{
         const data = await Post.findOneAndDelete({_id: mongoose.Types.ObjectId(userId)})
-        res.status(201).json(data)
+        res.status(201).send("Post deleted Successfully!")
     } catch (error){
         const errors= handleErrors(error)
         res.status(401).json({errors})
     }
 }
 
-async function editPost(userObj){
+async function addLike(userObj, postId, res){
     try{
         const data = await Post.updateOne({
-                _id: mongoose.Types.ObjectId(userObj.id)
+                _id: mongoose.Types.ObjectId(postId.trim())
             }, 
             {
-                $push:{
+                $addToSet:{
                 usersWhoLiked:{
                     username: userObj.username,
                     id: mongoose.Types.ObjectId(userObj.id)
@@ -88,6 +89,35 @@ async function editPost(userObj){
         res.status(201).json(data)
     } catch (error){
         const errors= handleErrors(error)
+        res.status(401).json({errors})
+    }
+}
+
+async function removeLike(userObj, postId, res){
+    try{
+        const data = await Post.updateOne({
+                _id: mongoose.Types.ObjectId(postId.trim())
+            }, 
+            {
+                $pull:{
+                usersWhoLiked:{
+                    username: userObj.username,
+                    id: mongoose.Types.ObjectId(userObj.id)
+                }}
+        })
+        res.status(201).json(data)
+    } catch (error){
+        const errors= handleErrors(error)
+        res.status(401).json({errors})
+    }
+}
+
+async function editPost(userObj, postId, res){
+    try{
+        const edit= await Post.findOneAndUpdate({_id:mongoose.Types.ObjectId(postId)}, {caption: userObj.caption})
+        res.status(201).send("Caption edited successfully!")
+    } catch(error){
+        const errors=handleErrors(error)
         res.status(401).json({errors})
     }
 }
